e921e4808763d4933f8c632a24912f1f74bcf482
Piyush Mehta <me@piyushmehta.com>
2019-01-23 09:49:29 +0530

    Initialization

--- GetMeFood/Screens/Details/Views/DetailsFoodView.swift
+++ GetMeFood/Screens/Details/Views/DetailsFoodView.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
 @IBDesignable class DetailsFoodView: BaseView{
-    @IBOutlet weak var collectionView: UICollection!
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
-    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var ratingsLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
 }
